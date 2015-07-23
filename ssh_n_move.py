df0d15e38950404b427662375758d43b7d50469a
woook <dev@stickie.be>
2015-07-23 20:25:46 +0100

    initial scripts

--- ssh_n_move.py
+++ ssh_n_move.py
@@ -1,1 +1,21 @@
+# python 2.7
+#
+# script in draft as a windows compatible one was required and pxssh doesn't exist for windows
+# therefore script is function but not finished
 
+import pxssh
+# import getpass
+try:
+    s = pxssh.pxssh()
+    # hostname = raw_input('hostname: ')
+    # username = raw_input('username: ')
+    # password = getpass.getpass('password: ')
+    # s.login (hostname, username, password)
+    s.login("athena.kcl.ac.uk", "ryank", "password")
+    s.sendline('mv /gpfs/home/ryank/NGS_runs/testdir* /gpfs/home/ryank/NGS_runs/testdir2')   # run a command
+    s.prompt()             # match the prompt
+    print s.before          # print everything before the prompt.
+    s.logout()
+except pxssh.ExceptionPxssh, e:
+    print "pxssh failed on login."
+    print str(e)
--- ssh_n_move_win.py
+++ ssh_n_move_win.py
@@ -1,1 +1,25 @@
+# python 2.7
+#
+# this is the windows version of ssh_n_move as pxssh does not exist for windows
+#
+# required modules: paramiko and pycrypto
+#
+# the later can apparently be a pain to install and so instead of installing python, have installed anaconda, then run:
+# conda install paramiko
+# conda install pycrytpo (although this should be installed automatically)
+#
+# todo: find a secure method for storing the password
+# todo: log successful mv
 
+import paramiko
+
+# ssh client
+ssh = paramiko.SSHClient()
+# auto accept host key without prompting and requiring response from a user
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# connect to server
+# ssh.connect('athena.kcl.ac.uk', username='ryank', password='password')
+ssh.connect('5.2.65.31', port=400, username='wook', password='password')
+# send move command
+# stdin, stdout, stderr = ssh.exec_command('mv /gpfs/home/ryank/NGS_runs/testdir2/* /gpfs/home/ryank/NGS_runs/testdir')
+stdin, stdout, stderr = ssh.exec_command('mv /home/wook/t1/* /home/wook/t2')
